663015fc65adb1717aeaa373daa9cca8ba751a00
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-20 20:04:04 +0200

    Merge EGL and GLES libraries into single library
    This is just to ease the burder of having to manage state across
    two seperate libraries till a later date when I can figure out
    how to do that.
    
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cargo build
 
 rm -f tester/main
 
-gcc -L$(pwd)/target/debug -o tester/main tester/main.c -legl -lgles
+gcc -L$(pwd)/target/debug -o tester/main tester/main.c -lgl
 
 LB_LIBRARY_PATH=$(pwd)/target/debug:$LD_LIBRARY_PATH ./tester/main
