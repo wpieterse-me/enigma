307f37797900dbec5d28bb1cedfe3b773146270c
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-20 23:34:07 +0200

    Align GL error codes with specification
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- gl/src/gles.rs
+++ gl/src/gles.rs
@@ -1,15 +1,18 @@
 #[repr(u32)]
 pub enum ErrorCode {
-    Success = 0,
+    NoError = 0,
     InvalidEnumeration = 0x500,
     InvalidValue = 0x501,
     InvalidOperation = 0x502,
     StackOverflow = 0x503,
     StackUnderflow = 0x504,
     OutOfMemory = 0x505,
+    // TODO(wpieterse): This is defined in the spec, but there is no definition
+    // of it inside the header.
+    // TableTooLarge,
 }
 
 #[no_mangle]
 pub extern "C" fn glGetError() -> ErrorCode {
-    ErrorCode::Success
+    ErrorCode::NoError
 }
