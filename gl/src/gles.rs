e493607aaf3dd257ccdd0374dba6a9648cf16d9d
Wynand Pieterse <wpieterse2825@gmail.com>
2023-05-20 20:25:39 +0200

    Add extra OpenGL error enumerations with comments
    Signed-off-by: Wynand Pieterse <wpieterse2825@gmail.com>

--- gl/src/gles.rs
+++ gl/src/gles.rs
@@ -1,11 +1,24 @@
 #[repr(u32)]
 pub enum ErrorCode {
+    /// The operation has completed successfully
     Success = 0,
+
+    /// Enumeration argument is out of range
     InvalidEnumeration = 0x500,
+
+    /// Numeric argument is out of range
     InvalidValue = 0x501,
+
+    /// The requested operation is illegal in the current state
     InvalidOperation = 0x502,
+
+    /// The requested operation would cause a stack overflow
     StackOverflow = 0x503,
+
+    /// The requested operation would cause a stack underflow
     StackUnderflow = 0x504,
+
+    /// Not enough memory is available to execute the requested operation
     OutOfMemory = 0x505,
 }
 
